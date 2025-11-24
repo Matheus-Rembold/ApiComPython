f6b8da8c36df799bc9c19eda8dc05b5fd05d042c
matheus <matheus11rembold@gmail.com>
2025-11-24 20:18:21 -0300

    aula dia 24/11

--- app/main.py
+++ app/main.py
@@ -2,9 +2,11 @@ from fastapi import FastAPI
 from .database import Base, engine
 from .routers import carros, servicos
 
-Base.metadata.create_all(bind=engine)
-
 app = FastAPI()
+
+@app.on_event("startup")
+def startup():
+    Base.metadata.create_all(bind=engine)
 
 app.include_router(carros.router)
 app.include_router(servicos.router)
